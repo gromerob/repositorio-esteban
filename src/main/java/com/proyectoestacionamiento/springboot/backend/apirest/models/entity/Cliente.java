9dadbdb3c0a9b61671855de5124caa85e567ddf1
Giovanni Romero <giovanni.romero1701@alumnos.ubiobio.cl>
2022-10-29 00:46:10 -0300

    creacion inicial de cliente

---
+++ src/main/java/com/proyectoestacionamiento/springboot/backend/apirest/models/entity/Cliente.java
@@ -0,0 +1,32 @@
+package com.proyectoestacionamiento.springboot.backend.apirest.models.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name="cliente")
+public class Cliente {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotEmpty
+	@Column(nullable = false, unique = true)
+	private String nombre;
+	
+	
+	
+
+}
